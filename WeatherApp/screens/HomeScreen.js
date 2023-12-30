be85ad8b9f393174f37ce372697b7b3d97cf0b5d
willianctti <willianctti@gmail.com>
2023-12-30 11:00:25 -0300

    ok

--- WeatherApp/screens/HomeScreen.js
+++ WeatherApp/screens/HomeScreen.js
@@ -1,16 +1,36 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text, Image } from "react-native";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+import { ActivityIndicator } from "react-native";
+
 
 export default function HomeScreen() {
+    const [loading, setLoading] = useState(true)
+    const [dados, setDados] = useState([])
+
+    useEffect(
+        () => {
+            fetch('https://randomuser.me/api/?results=100')
+                .then(res => resp.json())
+                .then( res => {
+                    this.setDados ({
+                        data: res.results
+                    })
+                })
+                .catch(()=>(alert('Error ao carregar users')))
+                .finally(() => setLoading(false))
+        }, []
+    )
+
+
     return (
         <View className="flex-1 relative">
-            <StatusBar style="light" />
             <Image style={styles.image} source={require('../assets/images/bg.png')} />
-
-
+            {
+                // loading ? <ActivityIndicator />
+            }
         </View>
     )
 }
@@ -19,6 +39,5 @@ const styles = StyleSheet.create({
     image: {
         height: '100%',
         width: '100%',
-        blurRadius: 1
     }
 })
